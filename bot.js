d8b0dabf9f834d37e238d7101ff0d7a731f19c4f
jamalbabia <47610404+jamalbabia@users.noreply.github.com>
2019-02-22 05:45:11 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1473,39 +1473,6 @@ client.on("message", message => {
   
   });
 
-client.on('message', async message => {
-    
-    let args = message.content.split(' ').slice(1);
-if (message.content.startsWith("^^delete colors")) {
-if(!message.member.hasPermission('ADMINISTRATOR')) return
-let role = message.guild.roles.find('name', 'رقم الون');
-
-role.delete()
-}
-
-});
-
-client.on('message', function(message) {
-    if (!message.member.hasPermissions(['ADMINISTRATOR'])){
-            let command = message.content.split(" ")[0];
-        if(message.content.includes('discord.gg')){
-        message.reply (' ')
-           if(!message.channel.guild) return message.reply('** This command only for servers**');
-     message.member.addRole(message.guild.roles.find('name', 'Muted')); 
-    const embed500 = new Discord.RichEmbed()
-      .setTitle(":x: | تمت معاقبتك")
-            .addField(`** لقد قمت بمخالفة قوانين السيرفر من خلال نشر سيرفرات اخرى  **` , `**ملاحظة  : إن كآن هذآ الميوت عن طريق الخطأ تكلم مع الادآرة**`)
-      .addField(`by`,`shyboy_05`)
-            .setColor("c91616")
-            .setThumbnail(`${message.author.avatarURL}`)
-            .setAuthor(message.author.username, message.author.avatarURL) 
-        .setFooter(`${message.guild.name} Server`)
-     message.channel.send(embed500) 
-    
-        
-    }
-    }
-})
 
 
 client.login(process.env.TOKEN);
